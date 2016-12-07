9b87af20c69da36847687e1b9a87a401a037f1d6
Alexandre Thorez <alexandrethorez@laposte.net>
2016-12-07 19:54:32 +0100

    finish affichage

--- src/affichage.js
+++ src/affichage.js
@@ -1,39 +1,53 @@
 class Affichage {
 
-    constructor(id) {
+    constructor(id, name) {
         this.tableau = document.getElementById(id);
+        this.name = name;
     }
 
-    createArray() {
+    handleEvent() {
+        supprimerTableau(this.name, this.clearArray());
+    }
+
+    getData() {
+        var context = this;
+        var table = this.tableau.appendChild(document.createElement("table"));
+        table.className = "table table-striped table-bordered";
+        avoirToutLeTableau(this.name, function(data) {
+            context.createArray(table, data);
+        });
+    }
+
+    createArray(table, data) {
         var tableau;
         var tr, trhead, td, th, div, tbody, thead;
         var creneauxCourant;
-        var id = this.tableau;
-        var table = id.appendChild(document.createElement("table"));
-        table.className = "table table-striped";
-        avoirToutLeTableau("tableauAlex", function(json) {
-            tableau = json.donnees.tableau;
+        tableau = data.donnees.tableau;
 
-            thead = table.appendChild(document.createElement("thead"));
-            trhead = thead.appendChild(document.createElement("tr"));
+        thead = table.appendChild(document.createElement("thead"));
+        trhead = thead.appendChild(document.createElement("tr"));
 
-            for (var item in tableau[0]) {
-                th = trhead.appendChild(document.createElement("th"));
-                th.setAttribute("class", "col-lg-3");
-                th.innerHTML = item;
+        trhead.appendChild(document.createElement("th")).innerHTML = "#";
+
+        for (var item in tableau[0]) {
+            th = trhead.appendChild(document.createElement("th"));
+            th.innerHTML = item;
+        }
+
+        tbody = table.appendChild(document.createElement("tbody"));
+
+        for (var i = 0; i < tableau.length; i++) {
+            tr = tbody.appendChild(document.createElement("tr"));
+            creneauxCourant = tableau[i];
+            tr.appendChild(document.createElement("th")).innerHTML = i;
+            for (var item in creneauxCourant) {
+                td = tr.appendChild(document.createElement("td"));
+                td.innerHTML = creneauxCourant[item];
             }
+        }
+    }
 
-            tbody = table.appendChild(document.createElement("tbody"));
-
-            for (var i = 0; i < tableau.length; i++) {
-                tr = tbody.appendChild(document.createElement("tr"));
-                creneauxCourant = tableau[i];
-                for (var item in creneauxCourant) {
-                    td = tr.appendChild(document.createElement("td"));
-                    td.setAttribute("class", "col-lg-3");
-                    td.innerHTML = creneauxCourant[item];
-                }
-            }
-        });
+    clearArray() {
+        this.tableau.innerHTML = "";
     }
 }
